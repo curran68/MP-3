19ca28aa00efacc5817e38f91d4d0d6ed72830a1
curran68 <mac1968@gmail.com>
2025-11-25 20:25:53 +0000

    updates

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $("select").formSelect();
+  });
+
+  
